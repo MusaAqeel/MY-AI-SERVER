7c9a4e26e4c5dc8f749da898f1e35bd97becf702
Daniel Martinez <daniellhf@icloud.com>
2023-01-23 18:00:48 -0500

    initial commit

---
+++ api/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
+const port = 3001;
+require("dotenv").config({ path: require("find-config")(".env") });
+const cors = require("cors");
+app.use(cors());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+const { Configuration, OpenAIApi } = require("openai");
+
+const configuration = new Configuration({
+  organization: "org-7EI8r48srsW3pVpQ7E7KPmy6",
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const dialogExample = [
+  {
+    speaker: "user",
+    text: "Hello, how are you?",
+  },
+  {
+    speaker: "bot",
+    text: "I am doing well, thank you. How can I help you today?",
+  },
+];
+
+async function request(req) {
+  // console.log(req)
+  const completion = await openai.createCompletion({
+    model: "text-davinci-002",
+    prompt: message,
+    temperature: 0.5,
+    max_tokens: 60,
+    top_p: 1.0,
+    frequency_penalty: 0.5,
+    presence_penalty: 0,
+  });
+  return { result: completion.data.choices[0].text };
+}
+
+app.get("/api", (req, res) => {
+  res.send("make a post request to get the response");
+});
+
+app.post("/api", (req, res) => {
+  console.log(req.body);
+  console.log(process.env.OPENAI_API_KEY);
+  // res.send("hi")
+  // let output = request().then((result) => console.log(result))
+  let output = null;
+  request(req.body)
+    .then((result) => {
+      res.json(result);
+    })
+    .then((data) => {
+      output = data;
+      console.log(data);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
