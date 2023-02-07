56b60d019c95854bb880639623e4fc998e5fcbb7
Musa <77996753+MusaAqeel@users.noreply.github.com>
2023-02-07 12:47:40 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -37,7 +37,7 @@ async function request(req) {
     model: "text-davinci-003",
     prompt: req.message,
     temperature: 0.5,
-    max_tokens: 60,
+    max_tokens: 100,
     top_p: 1.0,
     frequency_penalty: 0.5,
     presence_penalty: 0,
